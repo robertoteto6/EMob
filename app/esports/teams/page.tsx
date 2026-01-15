a0639577015d5c257841c90381d290fbe176c8ca
Roberto Muñoz Plaza <robertoteto6@gmail.com>
2026-01-15 18:09:05 +0100

    brereg

---
+++ app/esports/teams/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation";
+
+type SearchParams = Record<string, string | string[] | undefined>;
+
+function buildQuery(searchParams: SearchParams) {
+  const params = new URLSearchParams();
+  for (const [key, value] of Object.entries(searchParams)) {
+    if (Array.isArray(value)) {
+      value.forEach((item) => params.append(key, item));
+    } else if (value !== undefined) {
+      params.set(key, value);
+    }
+  }
+  const query = params.toString();
+  return query ? `?${query}` : "";
+}
+
+export default function TeamsRedirect({
+  searchParams = {},
+}: {
+  searchParams?: SearchParams;
+}) {
+  redirect(`/equipos${buildQuery(searchParams)}`);
+}
