3278687941a7485add85bc41a314544ccf9742fd
Roberto Muñoz Plaza <robertoteto6@gmail.com>
2026-01-22 19:21:40 +0100

    Update MobileGestures.tsx

--- app/components/MobileGestures.tsx
+++ app/components/MobileGestures.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { usePullToRefresh, useScrollIndicator } from '../hooks/usePullToRefresh';
 import { Spinner } from './LoadingOptimized';
 
